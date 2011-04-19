2a48e068e2bc98be551437bed82c4bf920d11150
Brian George <brianwork@brianandsherri.com>
2011-04-19 12:32:25 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TGData::Application.config.secret_token = 'acb7cd4980ec8acabc2291ed01cbdbd0d5ff240aceeb1e95ed53a8d630b4662a9c0cbec4303a4d82bd823343e88dd417c0927e9a7e01460c00a1cd4766088dba'
